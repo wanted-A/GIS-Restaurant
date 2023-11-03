17d84c2bbf1fa275c84fcc769613f21c153e7ffa
kyeon06 <jkyeon06@gmail.com>
2023-11-03 15:57:16 +0900

    :sparkles: feat : data pipline 수정

--- data_pipline.py
+++ data_pipline.py
@@ -55,7 +55,7 @@ def get_page_count(api_url, business_type):
     response = requests.get(api_url)
 
     list_total_count = response.json()[business_type][0]["head"][0]["list_total_count"]
-    total_page = math.ceil(list_total_count/100) # 총 페이지 수(페이지당 100개씩 보여질 경우)
+    total_page = math.ceil(list_total_count/1000) # 총 페이지 수(페이지당 100개씩 보여질 경우)
 
     return total_page
 
@@ -67,10 +67,10 @@ def request_data(business_type):
     
     total_raw_data = []
     for page in range(1, total_page + 1):
-        url = f'{api_url}&pIndex={page}'
+        url = f'{api_url}&pIndex={page}&pSize=1000'
         page_response = requests.get(url)
         raw_data = page_response.json()[business_type][1]["row"]
-        total_raw_data += raw_data
+        total_raw_data.extend(raw_data)
     return total_raw_data
 
 def data_processing(raw_data):
@@ -80,57 +80,52 @@ def data_processing(raw_data):
 
     # null 값 처리
     for field in FIELDS:
-        # float
-        if field in ["LOCPLC_AR", "TOT_FACLT_SCALE"]:
-            df[field] = df[field].astype(float)
-            df[field] = df[field].fillna(0.0)
-        # int
-        elif field in ["MALE_ENFLPSN_CNT", "FEMALE_ENFLPSN_CNT", "TOT_EMPLY_CNT"]:
-            df[field] = pd.to_numeric(df[field])
-            df[field] = df[field].fillna(0)
-        # YN
-        elif field in ["MULTI_USE_BIZESTBL_YN"]:
-            df[field] = df[field].fillna("N")
-        # date
-        elif field in ["LICENSG_DE", "CLSBIZ_DE"]:
-            df[field] = pd.to_datetime(df[field])
-            df[field] = df[field].dt.strftime('%Y-%m-%d')
-            df[field] = df[field].fillna(" ")
-        # string
-        else:
-            # 양 끝 공백 제거
-            df[field] = df[field].str.strip()
-            df[field] = df[field].fillna(" ")
-    
-    # 내부 필드명으로 변경
+        try:
+            if field in ["LOCPLC_AR", "TOT_FACLT_SCALE", "MALE_ENFLPSN_CNT", "FEMALE_ENFLPSN_CNT", "TOT_EMPLY_CNT","REFINE_WGS84_LOGT", "REFINE_WGS84_LAT"]:
+                df[field] = df[field].fillna(0)
+                df[field] = pd.to_numeric(df[field])
+            elif field in ["MULTI_USE_BIZESTBL_YN"]:
+                df[field] = df[field].fillna("N")
+            elif field in ["LICENSG_DE", "CLSBIZ_DE"]:
+                df[field] = df[field].replace("-","")
+                df[field] = df[field].fillna(" ")
+            else:
+                df[field] = df[field].str.strip()
+                df[field] = df[field].fillna(" ")
+        except Exception as e:
+            print("전처리 exception : " + e)
+            continue
+
+    # 내부 필드명으로 mapping
     df.rename(columns=FIELDS, inplace=True)
 
-    # 유일키 생성
-    df['restaurant_code'] = df["restaurant_name"] + df["road_address"]
+    mapped_data = df.to_dict('records')
 
-    # dataframe to json 저장
-    # df.to_json('./data/json_data.json',orient='records', force_ascii=False, indent=4)
+    return mapped_data
+  
 
-    data = df.to_dict('records')
-
-    return data
-
-def save_place_data(data):
+def save_raw_data(mapped_data):
     """ 데이터 DB에 저장 """
-    for row in data:  
+    for data in mapped_data:  
         try:
-            Restaurant(**row).save()
+            # 유일키 생성
+            restaurant_code = data["restaurant_name"] + "|" + data["road_address"]
+            obj, created = Restaurant.objects.update_or_create(defaults=data, restaurant_code=restaurant_code)
+            
+            # updated_data = []
+            # if not created:
+            #     updated_data.append(obj.__dict__)
+            
         except Exception as e:
-            print(row)
+            print("저장 exception : " + e)
             continue
 
 def run():
-    # for business_type in BUSINESS_TYPE:
-
-    # test: 이동음식
-    raw_data = request_data("Genrestrtmovmntcook")
-    data = data_processing(raw_data)
-    save_place_data(data)
+    Restaurant.objects.all().delete()
+    for business_type in BUSINESS_TYPE:
+        raw_data = request_data(business_type) # 데이터 수집
+        mapped_data = data_processing(raw_data) # 데이터 전처리 및 내부 필드명으로 mapping
+        save_raw_data(mapped_data) # DB에 데이터 저장
 
 if __name__ == "__main__":
     run()
