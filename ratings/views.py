d108f1dc2cd09c5673925c1678de69feee320265
kyeon06 <jkyeon06@gmail.com>
2023-11-05 22:44:11 +0900

    :bug: fix : user, restaurant String 반환으로 변경

--- ratings/views.py
+++ ratings/views.py
@@ -2,28 +2,30 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
-from rest_framework.exceptions import NotFound
 from ratings.models import Rating
 
 from ratings.serializers import RatingSerializer
 from restaurants.models import Restaurant
 
 
+# api/v1/ratings/<int:restaurant_id>/review/
 class ReviewAPIView(APIView):
 
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self, restaurant_id):
         return Restaurant.objects.get(id=restaurant_id)
+    
+    def get(self, request, restaurant_id):
+        return Response(
+            {"message" : "content(선택), score(필수) 를 입력해주세요."},
+            status=status.HTTP_200_OK,
+        )
 
-    # api/v1/ratings/<int:restaurant_id>/review/
     def post(self, request, restaurant_id):
         """
         평가 생성 API
         """
-        request.data["restaurant"] = restaurant_id
-        request.data["user"] = request.user.id
-
         serializer = RatingSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -31,7 +33,7 @@ class ReviewAPIView(APIView):
             restaurant = self.get_object(restaurant_id)
             review_count = Rating.objects.filter(restaurant=restaurant).count()
             
-            new_review = serializer.save()
+            new_review = serializer.save(user=request.user, restaurant=restaurant)
 
             restaurant.rating = ((restaurant.rating * review_count) + new_review.score) / (review_count + 1)
             restaurant.save()
