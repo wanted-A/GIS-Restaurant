7a44bc3d16d4923a04bbd5860189be45aec6486d
전정헌 <69235039+allen9535@users.noreply.github.com>
2023-11-06 23:09:20 +0900

    [기능 추가] 사용자 위치 기반 주변 음식점 목록 제공 기능 추가 (#32)

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from restaurants.views import RestaurantAPIView, RestaurantDetailAPIView
+from restaurants.views import (
+    RestaurantAPIView,
+    RestaurantDetailAPIView,
+    RestaurantListAPIView,
+)
 
 urlpatterns = [
     path("<int:restaurant_id>/", RestaurantAPIView.as_view(), name="restaurant-info"),
-    path("detail/<int:restaurant_id>/", RestaurantDetailAPIView.as_view(), name="restaurant-detail"),
+    path(
+        "detail/<int:restaurant_id>/",
+        RestaurantDetailAPIView.as_view(),
+        name="restaurant-detail",
+    ),
+    path("list/", RestaurantListAPIView.as_view(), name="restaurant_list"),
 ]
