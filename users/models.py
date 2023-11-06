ea27285c8dfb5f12f42ca791aad545168af252a3
ssu-uky <id_suhyun@naver.com>
2023-11-06 19:32:50 +0900

    :sparkles: feat : pk에 uuid고유번호 지정, 개인정보 수정되는 항목 변경

--- users/models.py
+++ users/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from .manager import CustomUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(
+        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+    )
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     objects = CustomUserManager()
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
-    
-    user_lat = models.FloatField(null=True, blank=True)  # 위도
-    user_lon = models.FloatField(null=True, blank=True)  # 경도
-    is_recommend = models.BooleanField(default=True, blank=True)  # 점심추천여부
-    
+
+    user_lat = models.FloatField(null=True, blank=True, default=0.0)  # 위도
+    user_lon = models.FloatField(null=True, blank=True, default=0.0)  # 경도
+    is_recommend = models.BooleanField(default=True, blank=True)  # 점심 추천 여부
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
