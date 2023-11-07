6ab539ca4d487e4ba0004a1c3a891f8ed00ebd62
kyeon06 <jkyeon06@gmail.com>
2023-11-08 01:06:49 +0900

    :truck: build : postgresql db 설정

--- config/settings.py
+++ config/settings.py
@@ -84,14 +84,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
-
-# DATABASES = my_settings.DATABASES
 
 
 # Password validation
