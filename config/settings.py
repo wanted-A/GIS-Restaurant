d5020897b3317e1c529c93e77a024b0d88a5b8db
kyeon06 <jkyeon06@gmail.com>
2023-11-08 02:03:00 +0900

    :sparkles: feat : 시군구 목록 조회 캐싱 적용

--- config/settings.py
+++ config/settings.py
@@ -198,3 +198,13 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(minute="*/5"),  # 5분마다 실행
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
--- location/views.py
+++ location/views.py
@@ -5,11 +5,18 @@ from rest_framework.views import APIView
 from location.models import Location
 from location.serializers import LocationSerializer
 
+from django.core.cache import cache
 
 # GET api/v1/restaurants/location/
 class LocationListAPIView(APIView):
     
     def get(self, request):
-        queryset = Location.objects.all()
-        serializer = LocationSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # cache에 저장된 데이터가 없을 경우 실행
+        if not cache.get('location_data'):
+            queryset = Location.objects.all()
+            serializer = LocationSerializer(queryset, many=True)
+            cache.set('location_data', serializer.data)
+            
+        data = cache.get('location_data')
+        return Response(data, status=status.HTTP_200_OK)
